e3e5be2145e00881f15212d8fee0cd2b134623be
HugoTalledos <hugo.talledos@bpsmart.ai>
2022-04-17 21:13:42 -0500

    General styles and minor changes added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import Button from './components/Button/Button';
+import ImagePicker from './components/ImagePicker/ImagePicker';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button label='Prueba' onClick={() => console.log("HOLAAAA")} trailingIcon='account'/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <Button label='Prueba' onClick={() => console.log("HOLAAAA")} trailingIcon='account'/> */}
+        {/* <ImagePicker 
+          multiple
+          maxSize={5}
+          maxSizeMB={5000000}
+          onChange={(e) => onChangeFile(e)}
+          imageList={imageList}
+          /> */}
       </header>
     </div>
   );
