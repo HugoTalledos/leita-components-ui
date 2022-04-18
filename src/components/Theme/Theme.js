d32b4cf415119fda0941db95579cad306fc3bd7d
HugoTalledos <hugo.talledos@bpsmart.ai>
2022-04-18 18:58:17 -0500

    Add theme component

---
+++ src/components/Theme/Theme.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import '@mdi/font/css/materialdesignicons.min.css';
+import PropTypes from 'prop-types';
+
+export const Theme = ({
+  children,
+  backgroundColor,
+  primary,
+  darkPrimary,
+  accent,
+  success,
+  warning,
+  danger,
+  white,
+  black,
+  shadow,
+  fontFamily,
+}) => {
+  return (<div>
+    {children}
+    <style jsx global>{`
+      :root {
+        --primary-color: ${primary};
+        --primary-dark: ${darkPrimary};
+        --light: ${white};
+        --dark: ${black};
+        --shadow:${shadow};
+        --accent: ${accent};
+        --success: ${success};
+        --warning: ${warning};
+        --danger: ${danger};
+        --glass-color: rgba( 255, 255, 255, 0.15 );
+        --glass-default-notify: #FFFBFCA1;
+      }
+      * {
+        font-family: ${fontFamily.join(', ')}, sans-serif;
+      }
+      body {
+        background-color: ${backgroundColor};
+      }
+    `}</style>
+  </div>)
+}
+
+Theme.protoType = {
+  children: PropTypes.oneOfType([PropTypes.object, PropTypes.arrayOf(PropTypes.element)]),
+};
