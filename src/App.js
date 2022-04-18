25b535d33034f9a835376bcb5b09b6579606ea29
HugoTalledos <hugo.talledos@bpsmart.ai>
2022-04-18 15:23:36 -0500

    Minor fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <List loading={test}>
+        <List loading>
           <ListItem 
             label="Pizza"
             value="1"
