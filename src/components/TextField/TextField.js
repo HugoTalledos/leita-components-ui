8d8a5ac376b5a2b27a5e987f1cfbf34b17312447
HugoTalledos <hugo.talledos@bpsmart.ai>
2022-04-19 00:01:35 -0500

    TextField added

---
+++ src/components/TextField/TextField.js
@@ -0,0 +1,76 @@
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import './TextField.css';
+
+export const TextArea = ({
+  label = '',
+  value,
+  type,
+  onChange,
+  id
+}) => {
+  useEffect(() => {
+    const input = document.getElementById(id);
+    input.onfocus = () => {
+      input.previousElementSibling.classList.add('top');
+      input.previousElementSibling.classList.add('focus');
+      input.parentNode.classList.add('focus');
+    };
+  
+    input.onblur = () => {
+      if (input.value.trim().length === 0) {
+        input.previousElementSibling.classList.remove('top');
+      }
+      input.previousElementSibling.classList.remove('focus');
+      input.parentNode.classList.remove('focus');
+    };
+  }, [id]);
+
+  return (
+  <div className='text__field'>
+    <label htmlFor={id}>
+      <span>{label}</span>
+      <textarea className='form-content' type={type} value={value} onChange={onChange} id={id} />
+    </label>
+  </div>);
+}
+
+export const TextField = ({
+  label = '',
+  value,
+  type,
+  onChange,
+  id
+}) => {
+  useEffect(() => {
+    const input = document.getElementById(id);
+    input.onfocus = () => {
+      input.previousElementSibling.classList.add('top');
+      input.previousElementSibling.classList.add('focus');
+      input.parentNode.classList.add('focus');
+    };
+  
+    input.onblur = () => {
+      if (input.value.trim().length === 0) {
+        input.previousElementSibling.classList.remove('top');
+      }
+      input.previousElementSibling.classList.remove('focus');
+      input.parentNode.classList.remove('focus');
+    };
+  }, [id]);
+
+  return (
+  <div className='text__field'>
+    <label htmlFor={id}>
+      <span>{label}</span>
+      <input type={type} value={value} onChange={onChange} id={id} />
+    </label>
+  </div>);
+}
+
+TextField.protoType = {
+  id: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+}
