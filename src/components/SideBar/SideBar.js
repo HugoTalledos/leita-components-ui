3934d7e8b7d5ab5d3f91f9775c44f829189d8f27
HugoTalledos <hugo.talledos@bpsmart.ai>
2022-04-18 21:47:52 -0500

    SideBar component  added

---
+++ src/components/SideBar/SideBar.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './SideBar.css';
+
+export const SideBarItem = ({ label, icon }) => (
+  <li>
+    <span className={`mdi mdi-24px mdi-${icon}`} />
+    <p>{label}</p>
+  </li>
+);
+
+export const SideBar = ({ children, title }) => {
+  const [view, setView] = useState(false);
+  return (<header>
+    <div className={'menu-navbar'}>
+      <span className='btn-menu' onClick={() => {console.log('hola?'); setView(!view)}}>
+        <span className={`mdi mdi-24px mdi-${view ? 'backburger' : 'menu'}`} /> {title}
+      </span>
+    </div>
+    <ul className={`view-${view}`}>
+      {children}
+    </ul>
+  </header>);
+};
+
+SideBarItem.propTypes = {
+  label: PropTypes.string,
+  icon: PropTypes.string,
+};
+
+SideBar.protoType = {
+  children: PropTypes.oneOfType([PropTypes.object, PropTypes.arrayOf(PropTypes.element)]),
+  title: PropTypes.string,
+};
