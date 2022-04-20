79ff1337047a9d70d2b0ccf5a6eda971d364aa5a
HugoTalledos <hugo.talledos@bpsmart.ai>
2022-04-20 00:01:22 -0500

    index file added

---
+++ src/components/index.js
@@ -0,0 +1,32 @@
+import { BottomNavigation, NavigationItem } from './BottomNavigation/BottomNavigation';
+import Button from './Button/Button';
+import { ChipList, ChipListItem } from './ChipList/ChipList';
+import ImagePicker from './ImagePicker/ImagePicker';
+import { List, ListItem } from './List/List';
+import MDIcon from './MDIcon/MDIcon';
+import { SideBar, SideBarItem } from './SideBar/SideBar';
+import { Tabs, TabItem } from './Tabs/Tabs';
+import { TextField, TextArea } from './TextField/TextField';
+import Theme from './Theme/Theme';
+import { ToggleButton, ToggleButtonItem } from './ToggleButton/ToggleButton';
+
+export {
+  BottomNavigation,
+  NavigationItem,
+  Button,
+  ChipList,
+  ChipListItem,
+  ImagePicker,
+  List,
+  ListItem,
+  MDIcon,
+  SideBar,
+  SideBarItem,
+  Tabs,
+  TabItem,
+  TextField,
+  TextArea,
+  ToggleButton,
+  ToggleButtonItem,
+  Theme,
+};
