2892cf7cf6aa384131ff2b8bdda43bcc05cb4a08
HugoTalledos <hugo.talledos@bpsmart.ai>
2022-04-24 17:59:53 -0500

    Index modified

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,9 @@
 export * from './Buttons';
 export * from './Fields';
-// export { default as ImagePicker } from './ImagePicker/ImagePicker';
-// export * from './Lists';
-// export { default as MDIcon } from './MDIcon/MDIcon';
-// export * from './Navigation';
-// export { default as Notification } from './Notification/Notification';
-// export * from './Tabs/Tabs';
-// export { default as Theme } from './Theme/Theme';
+export { default as ImagePicker } from './ImagePicker/ImagePicker';
+export * from './Lists';
+export { default as MDIcon } from './MDIcon/MDIcon';
+export * from './Navigation';
+export { default as Notification } from './Notification/Notification';
+export * from './Tabs/Tabs';
+export { default as Theme } from './Theme/Theme';
