b0442b1eca1dfa1c83c7d8e3ab42e102fdb575fc
HugoTalledos <hugo.talledos@bpsmart.ai>
2022-04-24 17:48:58 -0500

    Minor changes notification

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -13,10 +13,10 @@ const Notification: FC<NotificationProps> = ({
   <div>
     <div className={`${styles['notification-container']} ${show ? '' : styles['not-show']}`}>
       <div className={styles['notification-content']}>
-        <div className={`${styles['notification-container__icon']} ${type}`}>
+        <div className={`${styles['notification-container__icon']} ${styles[type]}`}>
           <MDIcon name={icon} />
         </div>
-        <div className={`${styles['notification-container__text']} ${type}`}>
+        <div className={`${styles['notification-container__text']} ${styles[type]}`}>
           <h5>{text}</h5>
           <p>{description}</p>
         </div>
