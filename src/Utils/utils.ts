3a4c0abafb64d926851c094fb7750fbe3d02d8a3
HugoTalledos <hugo.talledos@bpsmart.ai>
2022-06-03 19:11:54 -0500

    Util auto position tooltip added

--- src/Utils/utils.ts
+++ src/Utils/utils.ts
@@ -1,4 +1,5 @@
 import styles from '../components/Fields/Fields.module.css';
+import styleTooltip from '../components/Tooltip/Tooltip.module.css';
 
 export const moveLabel = (ref:any, value:string, disabled:boolean): void => {
   const node = ref.current;
@@ -25,3 +26,20 @@ export const moveLabel = (ref:any, value:string, disabled:boolean): void => {
       node.classList.remove(styles.focus);
     };
 }
+
+export const showTooltip = (ref:any, align: string): void => {
+  const node = ref.current;
+  if(!node) return;
+  const { bottom, right, top, width } = node.firstElementChild.getBoundingClientRect();
+
+  node.firstElementChild.classList.remove(styleTooltip.top);
+  node.firstElementChild.classList.remove(styleTooltip.bottom);
+  node.firstElementChild.classList.remove(styleTooltip.left);
+  node.firstElementChild.classList.remove(styleTooltip.right);
+
+  if (align !== 'auto') return node.firstElementChild.classList.add(styleTooltip[align]);
+  if ((window.innerWidth - right) <= width) return node.firstElementChild.classList.add(styleTooltip.left);
+  if (top < (window.innerHeight - bottom)) return node.firstElementChild.classList.add(styleTooltip.bottom);
+
+  return node.firstElementChild.classList.add(styleTooltip.top);
+};
