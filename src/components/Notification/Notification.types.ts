2e70defff0b0e217b7356fd6e0debd3ef211987a
HugoTalledos <hugo.talledos@bpsmart.ai>
2022-08-05 20:23:29 -0500

    Add view more and close button in notification

--- src/components/Notification/Notification.types.ts
+++ src/components/Notification/Notification.types.ts
@@ -1,7 +1,10 @@
+import { MouseEventHandler } from "react";
 export interface NotificationProps {
   text: string;
   description?: string;
   type?: 'success' | 'warning' | 'danger';
   icon?: string;
   show?: boolean;
+  onClose: MouseEventHandler<HTMLButtonElement>
+  maxLengthDescription: number,
 }
